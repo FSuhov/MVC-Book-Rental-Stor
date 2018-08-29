f7f125ac20feadcf60f27c618d239df4ce8af1c1
FSuhov <aksu@ukr.net>
2018-08-29 19:15:11 +0300

    Project created, frontend admin menu created (html + javascript), models for Book and Genre created

---
+++ AlexBeeBookRental/AlexBeeBookRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlexBeeBookRental.MvcApplication" Language="C#" %>
---
+++ AlexBeeBookRental/AlexBeeBookRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlexBeeBookRental.Startup))]
+namespace AlexBeeBookRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
